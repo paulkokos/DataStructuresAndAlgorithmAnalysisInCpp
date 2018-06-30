0736ad561c29d148f62619a9d4ee9652cd597ec1
paulkokos <paulkokos@yahoo.gr>
2018-06-30 23:43:01 +0300

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,69 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename Comparable>
+const Comparable & findMax(const vector<Comparable> &a)
+{
+	int maxIndex =0;
+	for (int i=1;i<a.size();++i) {
+		if (a[maxIndex] < a[i] ){
+			maxIndex=i;
+		}
+		return a[maxIndex];
+
+	}
+}
+//Class IntCell
+class IntCell {
+	public:
+		explicit IntCell(int initialValue = 0)
+		{ storedValue = new int{initialValue};}
+
+		~IntCell() {
+			delete storedValue;
+		}
+
+		IntCell(const IntCell &rhs){
+			storedValue = new int {*rhs.storedValue};
+		}
+
+		IntCell(IntCell && rhs) : storedValue {rhs.storedValue}
+		{rhs.storedValue = nullptr;}
+
+		IntCell & operator=(const IntCell & rhs) {
+			if (this != &rhs) {
+				*storedValue = *rhs.storedValue;
+			}
+			return *this;
+		}
+
+		IntCell & operator = (IntCell && rhs) {
+			std::swap(storedValue,rhs.storedValue);
+			return *this;
+		}
+
+		int read () const {
+			return *storedValue;
+		}
+		void write (int x) {
+			*storedValue = x;
+		}
+	private:
+		int *storedValue;
+};
+
 int main(){
-    std::cout << "hello world" << std::endl;
+    vector<int> v1 (60);
+    vector<double> v2 (60);
+    vector<string> v3 (60);
+    vector<IntCell> v4 (60);
+
+    cout << findMax(v1) << endl;
+    cout << findMax(v2) << endl;
+    cout << findMax(v3) << endl;
+    //cout << findMax(v4) << endl;
     return 0;
 }
