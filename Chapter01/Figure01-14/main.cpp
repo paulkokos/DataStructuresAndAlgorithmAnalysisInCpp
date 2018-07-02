97814a58caf3c0aad69e4311597fb35d73ca665f
paulkokos <paulkokos@yahoo.gr>
2018-07-02 05:08:15 +0300

    Figure01-14,Figure01-15 Completion

--- Chapter01/Figure01-14/main.cpp
+++ Chapter01/Figure01-14/main.cpp
@@ -4,33 +4,34 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void swap (double & x, double & y);
+void swap1 (double & x, double & y);
 
-void swap (vector<string>& x, vector<string> &y);
+void swap2 (vector<string>& x, vector<string> &y);
 
 
 int main(){
     double a=10.33,b=20.33;
     string string1 = "daidalos";
     string string2 = "daidaloukos";
-    swap(a,b);
-    swap(string1,string2);
+    swap1(a,b);
+    //swap2("daidalos","daidaloykos");
 
     cout << string1 << " " << string2 << endl;
     cout << a << " " << b << endl;
     return 0;
 }
 
-void swap (double & x, double & y) {
+void swap1 (double & x, double & y) {
     double tmp = x;
     x=y;
     y = tmp;
 }
 
-void swap (vector<string>& x, vector<string> &y){
+void swap2 (vector<string>& x, vector<string> &y){
     vector<string> tmp =x;
     x=y;
     y=tmp;
